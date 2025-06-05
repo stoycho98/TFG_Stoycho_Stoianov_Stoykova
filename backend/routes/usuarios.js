7f1c85571a6aeaddb99a74d12615e3e211fecaf4
Stoycho <stoycho98@gmail.com>
2025-06-05 11:18:08 +0200

    panel admin arreglado

--- backend/routes/usuarios.js
+++ backend/routes/usuarios.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const usuariosCtrl = require('../controllers/usuariosController');
 const verifyToken = require('../middlewares/verifyToken');
 
-router.get('/', usuariosCtrl.ObtenerTodosLosUsuarios);
 router.post('/', usuariosCtrl.crearUsuario);
 router.use(verifyToken);
+router.get('/', usuariosCtrl.ObtenerTodosLosUsuarios);
 router.put('/:id', usuariosCtrl.actualizarUsuario);
 
 module.exports = router;
